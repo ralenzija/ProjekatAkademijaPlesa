c2b8851c47e8f9a0a6050d3e7a9fb923918a0f81
Rastko <rastkolazarevic41@gmail.com>
2026-06-16 19:41:59 +0200

    ne pratimo vise config fajlove i dodati u gitignore

--- Maven-Projekti/Client/src/main/java/dance/academy/client/komunikacija/Komunikacija.java
+++ Maven-Projekti/Client/src/main/java/dance/academy/client/komunikacija/Komunikacija.java
@@ -20,27 +20,32 @@ import dance.academy.common.komunikacija.Odgovor;
 import dance.academy.common.komunikacija.Operacija;
 import dance.academy.common.komunikacija.Posiljalac;
 import dance.academy.common.komunikacija.Primalac;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+
 /**
  *
  * @author Korisnik
  */
 public class Komunikacija {
-    
+
     private Socket socket;
     private static Komunikacija instance;
     private Posiljalac posiljalac;
     private Primalac primalac;
 
     private Komunikacija() {
-        
+
     }
 
     public static Komunikacija getInstance() {
-        if (instance == null)
+        if (instance == null) {
             instance = new Komunikacija();
+        }
         return instance;
     }
-    
+
     public void konekcija() {
         try {
             socket = new Socket("localhost", 9000);
@@ -52,209 +57,207 @@ public class Komunikacija {
     }
 
     public Instruktor login(Instruktor i) {
-        
+
         Zahtev z = new Zahtev(Operacija.LOGIN, i);
         posiljalac.posalji(z);
-        
+
         Odgovor o = (Odgovor) primalac.primi();
         i = (Instruktor) o.getOdgovor();
-        
+
         return i;
-        
+
     }
 
     public List<Ucesnik> ucitajUcesnike() {
-        
-        List<Ucesnik> lista = new ArrayList<>();
-        
+
         Zahtev z = new Zahtev(Operacija.UCITAJ_UCESNIKE, null);
         posiljalac.posalji(z);
-        
+
         Odgovor o = (Odgovor) primalac.primi();
-        lista = (List<Ucesnik>) o.getOdgovor();
-        
-        return lista;
-        
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Ucesnik>>() {
+        }.getType();
+        return gson.fromJson(o.getJsonOdgovor(), type);
+
     }
 
     public List<Ucesnik> pretraziUcesnike(Ucesnik u) {
-        
-        List<Ucesnik> filtrirani = new ArrayList<>();
-        
+
         Zahtev z = new Zahtev(Operacija.PRETRAZI_UCESNIKE, u);
         posiljalac.posalji(z);
-        
+
         Odgovor o = (Odgovor) primalac.primi();
-        filtrirani = (List<Ucesnik>) o.getOdgovor();
-        
-        return filtrirani;
-        
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Ucesnik>>() {
+        }.getType();
+        return gson.fromJson(o.getJsonOdgovor(), type);
+
     }
-    
+
     public NivoVestine pronadjiNivoVestine(NivoVestine nv) {
-        
+
         Zahtev z = new Zahtev(Operacija.UCITAJ_NIVOE, nv);
         posiljalac.posalji(z);
-        
+
         Odgovor o = (Odgovor) primalac.primi();
         nv = (NivoVestine) o.getOdgovor();
-        
+
         return nv;
-        
+
     }
 
     public void kreirajUcesnika(Ucesnik u) {
-        
+
         NivoVestine nv = pronadjiNivoVestine(u.getNivo());
         u.setNivo(nv);
-        
-        List<Ucesnik> lista = ucitajUcesnike();
-        
+
         Zahtev z = new Zahtev(Operacija.KREIRAJ_UCESNIKA, u);
         posiljalac.posalji(z);
-        
+
         Odgovor o = (Odgovor) primalac.primi();
         boolean uspeh = (boolean) o.getOdgovor();
-        
-        if (uspeh)
+
+        if (uspeh) {
             System.out.println("Uspeh");
-        else
+        } else {
             System.out.println("Neuspeh");
-        
+        }
+
     }
 
     public void dodajSertifikat(Sertifikat s) {
-        
+
         Zahtev z = new Zahtev(Operacija.UBACI_SERTIFIKAT, s);
         posiljalac.posalji(z);
-        
+
         Odgovor o = (Odgovor) primalac.primi();
         boolean uspeh = (boolean) o.getOdgovor();
-        
-        if (uspeh)
+
+        if (uspeh) {
             System.out.println("Uspeh");
-        else
+        } else {
             System.out.println("Neuspeh");
-        
+        }
+
     }
 
     public void obrisiUcesnika(Ucesnik selektovani) {
-        
+
         Zahtev z = new Zahtev(Operacija.OBRISI_UCESNIKA, selektovani);
         posiljalac.posalji(z);
-        
+
         Odgovor o = (Odgovor) primalac.primi();
         boolean uspeh = (boolean) o.getOdgovor();
-        
-        if (uspeh)
+
+        if (uspeh) {
             System.out.println("Uspeh");
-        else
+        } else {
             System.out.println("Neuspeh");
-        
+        }
+
     }
 
     public void promeniUcesnika(Ucesnik uc) {
-        
+
         NivoVestine nv = pronadjiNivoVestine(uc.getNivo());
         uc.setNivo(nv);
-        
+
         Zahtev z = new Zahtev(Operacija.IZMENI_UCESNIKA, uc);
         posiljalac.posalji(z);
-        
+
         Odgovor o = (Odgovor) primalac.primi();
         boolean uspeh = (boolean) o.getOdgovor();
-        
-        if (uspeh)
+
+        if (uspeh) {
             System.out.println("Uspeh");
-        else
+        } else {
             System.out.println("Neuspeh");
-        
+        }
+
     }
 
     public List<Instruktor> ucitajInstruktore() {
-        
-        List<Instruktor> lista = new ArrayList<>();
-        
+
         Zahtev z = new Zahtev(Operacija.UCITAJ_INSTRUKTORE, null);
         posiljalac.posalji(z);
-        
+
         Odgovor o = (Odgovor) primalac.primi();
-        lista = (List<Instruktor>) o.getOdgovor();
-        
-        return lista;
-        
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Instruktor>>() {
+        }.getType();
+        return gson.fromJson(o.getJsonOdgovor(), type);
+
     }
 
     public List<UpisNaProgram> ucitajUpise() {
-        
-        List<UpisNaProgram> lista = new ArrayList<>();
-        
+
         Zahtev z = new Zahtev(Operacija.UCITAJ_UPISE, null);
         posiljalac.posalji(z);
-        
+
         Odgovor o = (Odgovor) primalac.primi();
-        lista = (List<UpisNaProgram>) o.getOdgovor();
-        
-        return lista;
-        
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<UpisNaProgram>>() {
+        }.getType();
+        return gson.fromJson(o.getJsonOdgovor(), type);
+
     }
 
     public List<UpisNaProgram> pretraziUpise(KriterijumPretrage kp) {
-        
-        List<UpisNaProgram> filtrirani = new ArrayList<>();
-        
+
         Zahtev z = new Zahtev(Operacija.PRETRAZI_UPISE, kp);
         posiljalac.posalji(z);
-        
+
         Odgovor o = (Odgovor) primalac.primi();
-        filtrirani = (List<UpisNaProgram>) o.getOdgovor();
-        
-        return filtrirani;
-        
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<UpisNaProgram>>() {
+        }.getType();
+        return gson.fromJson(o.getJsonOdgovor(), type);
+
     }
 
     public List<ProgramAktivnosti> ucitajPrograme() {
-        
-        List<ProgramAktivnosti> lista = new ArrayList<>();
-        
+
         Zahtev z = new Zahtev(Operacija.UCITAJ_PROGRAME, null);
         posiljalac.posalji(z);
-        
+
         Odgovor o = (Odgovor) primalac.primi();
-        lista = (List<ProgramAktivnosti>) o.getOdgovor();
-        
-        return lista;
-        
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<ProgramAktivnosti>>() {
+        }.getType();
+        return gson.fromJson(o.getJsonOdgovor(), type);
+
     }
 
     public void kreirajUpis(UpisNaProgram unp) {
-        
+
         Zahtev z = new Zahtev(Operacija.KREIRAJ_UPIS, unp);
         posiljalac.posalji(z);
-        
+
         Odgovor o = (Odgovor) primalac.primi();
         boolean uspeh = (boolean) o.getOdgovor();
-        
-        if (uspeh)
+
+        if (uspeh) {
             System.out.println("Uspeh");
-        else
+        } else {
             System.out.println("Neuspeh");
-        
+        }
+
     }
 
     public void izmeniUpis(UpisNaProgram zaIzmenu) {
-        
+
         Zahtev z = new Zahtev(Operacija.IZMENI_UPIS, zaIzmenu);
         posiljalac.posalji(z);
-        
+
         Odgovor o = (Odgovor) primalac.primi();
         boolean uspeh = (boolean) o.getOdgovor();
-        
-        if (uspeh)
+
+        if (uspeh) {
             System.out.println("Uspeh");
-        else
+        } else {
             System.out.println("Neuspeh");
-        
+        }
+
     }
 
     public void promeniStatusUpisa(UpisNaProgram up) {
@@ -270,19 +273,17 @@ public class Komunikacija {
             System.out.println("Neuspeh");
         }
     }
-    
-    public List<ProgramAktivnosti> preporuciProgram(ProgramAktivnosti filter) {
 
-        List<ProgramAktivnosti> lista = new ArrayList<>();
+    public List<ProgramAktivnosti> preporuciProgram(ProgramAktivnosti filter) {
 
         Zahtev z = new Zahtev(Operacija.PREPORUCI_PROGRAM, filter);
         posiljalac.posalji(z);
 
         Odgovor o = (Odgovor) primalac.primi();
-        lista = (List<ProgramAktivnosti>) o.getOdgovor();
-
-        return lista;
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<ProgramAktivnosti>>() {
+        }.getType();
+        return gson.fromJson(o.getJsonOdgovor(), type);
     }
-   
 
 }
--- Maven-Projekti/Common/src/main/java/dance/academy/common/komunikacija/Odgovor.java
+++ Maven-Projekti/Common/src/main/java/dance/academy/common/komunikacija/Odgovor.java
@@ -1,32 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dance.academy.common.komunikacija;
-
 import java.io.Serializable;
 
-/**
- *
- * @author Korisnik
- */
 public class Odgovor implements Serializable {
     
     private Object odgovor;
-
+    private String jsonOdgovor;
+    
     public Odgovor() {
     }
-
+    
     public Odgovor(Object odgovor) {
         this.odgovor = odgovor;
     }
-
+    
     public Object getOdgovor() {
         return odgovor;
     }
-
+    
     public void setOdgovor(Object odgovor) {
         this.odgovor = odgovor;
     }
     
+    public String getJsonOdgovor() {
+        return jsonOdgovor;
+    }
+    
+    public void setJsonOdgovor(String jsonOdgovor) {
+        this.jsonOdgovor = jsonOdgovor;
+    }
 }
